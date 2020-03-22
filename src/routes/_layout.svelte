2ea806c93236fa030e1fdff161a378734b09a5ed
Daniel Schischkin <daniel@schischkin.info>
2020-03-22 22:17:03 +0100

    Add TailwindCSS

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,20 +1,9 @@
 <script>
 	import Nav from '../components/Nav.svelte';
-
+	import Tailwind from '../components/Tailwind.svelte';
 	export let segment;
 </script>
-
-<style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-</style>
-
+<Tailwind/>
 <Nav {segment}/>
 
 <main>
